485c4931a1c142c782f7ff5227e45f6e9947f694
Volodymyr Voronov <malones.vladimir@gmail.com>
2023-09-11 19:45:10 +0200

    chore(cmd): updated main.

--- cmd/weather/main.go
+++ cmd/weather/main.go
@@ -1,20 +1,41 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
+	"weather-cli/models"
 	"weather-cli/pkg/api"
 
 	"github.com/fatih/color"
 )
 
 func main() {
-	color.Cyan("Prints text in cyan.")
+	var cityName string
+	var forecast models.ForecastDay
+	var err error
 
-	forecast, err := api.GetWeather("sasasrk")
+	scanner := bufio.NewScanner(os.Stdin)
+
+	color.Cyan("Enter the name of the city. Example: New-York")
+	color.Yellow("Note: If your city name contains spaces, please use dash.")
+	color.Magenta("----------------------------------")
+	color.Cyan("Your city:")
+
+	scanner.Scan()
+
+	if scanner.Err() != nil {
+		fmt.Println("Error: ", scanner.Err())
+	}
+
+	cityName = scanner.Text()
+
+	forecast, err = api.GetWeather(cityName)
 
 	if err != nil {
 		fmt.Println(err)
 	}
 
 	fmt.Println(forecast)
+
 }
